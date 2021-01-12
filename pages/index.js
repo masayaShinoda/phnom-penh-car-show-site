8e5b9affa0e64e24f9385047d3d388f2877811f6
Masaya Shida <masayashida@gmail.com>
2021-01-12 16:48:00 +0700

    minor style changes

--- pages/index.js
+++ pages/index.js
@@ -10,20 +10,27 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Nav></Nav>
-      <main className={styles.main} style={{ marginTop: `7.5vh` }}>
+      <main className={styles.main} style={{ marginTop: `12.5vh` }}>
         <h1 className={styles.title}>
           Cambodia's First Auto Event
         </h1>
 
         <p className={styles.description}>
-          At the heart of Phnom Penh, Diamond City.
+          At the heart of Phnom Penh, Diamond Island.
           <br></br>
-          1/1/2021
+          <span style={{ fontWeight: `800`}}>
+            1/1/2021
+          </span>
         </p>
+        <div style={{display: `flex`}}>
+          <a href="/" className={styles.card} style={{display: `flex`, justifyContent: `center`, alignItems: `center`,  textAlign: `center`, padding: `0.8rem`, margin: `0.5rem`}}>
+            <h3 style={{ margin: `0`, fontSize: `1.1rem` }}>Learn More</h3>
+          </a>
+          <a href="/" className={styles.card} style={{display: `flex`, justifyContent: `center`, alignItems: `center`,  textAlign: `center`, padding: `0.8rem`, margin: `0.5rem`}}>
+            <h3 style={{ margin: `0`, fontSize: `1.1rem` }}>Pricing</h3>
+          </a>
 
-        <a href="/" className={styles.card}>
-          <h3 style={{ margin: `0` }}>Learn More</h3>
-        </a>
+        </div>
 
       </main>
 
